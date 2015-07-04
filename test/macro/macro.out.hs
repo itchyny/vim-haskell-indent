f75b87b72239dc8daf65da6efd2927084d3713b5
itchyny <itchyny@hatena.ne.jp>
2015-07-05 00:28:21 +0900

    add tests

---
+++ test/macro/macro.out.hs
@@ -0,0 +1,12 @@
+#if MIN_VERSION_base(4,8,0)
+import Control.Applicative ((<$>))
+#else
+import Control.Applicative (Applicative(..), (<$>))
+import Data.Monoid (Monoid(..))
+import Data.Traversable (Traversable(traverse))
+#endif
+#if __GLASGOW_HASKELL__
+import GHC.Exts ( build )
+#if __GLASGOW_HASKELL__ >= 708
+import qualified GHC.Exts as GHCExts
+#endif
