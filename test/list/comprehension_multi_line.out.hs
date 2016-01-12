b417c28293e3190985698ae198170aa5bffa801d
itchyny <itchyny@hatena.ne.jp>
2016-01-12 09:18:22 +0900

    add tests for list comprehension

---
+++ test/list/comprehension_multi_line.out.hs
@@ -0,0 +1,7 @@
+z = (x, y)
+  where x = [ n * n | n <- [1..]
+                    , odd n
+                    , isPrime n ]
+        y = [ n * n | n <- [1..]
+                    , even n
+                    , isPrime (n `div` 2) ]
