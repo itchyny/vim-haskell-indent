de1918d74cda7140213101b077d8689fca5a964d
itchyny <itchyny@hatena.ne.jp>
2016-08-30 09:34:05 +0900

    add test for indent after let in

---
+++ test/case/case_let_in.out.hs
@@ -0,0 +1,9 @@
+h = f
+  where
+    f x = case x of
+               1 -> let y = g x
+                        in y
+               4 -> let y = g x
+                        in y
+               _ -> let y = g x
+                        in y
