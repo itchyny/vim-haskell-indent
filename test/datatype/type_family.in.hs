ddd1f4bae52d60151d8dd8cd3ff2d454c38c014a
itchyny <itchyny@hatena.ne.jp>
2017-05-15 20:40:55 +0900

    add tests for indentation of type family

---
+++ test/datatype/type_family.in.hs
@@ -0,0 +1,19 @@
+class X k => G k where
+data GM k :: * -> *
+a :: GM k v
+
+instance X Int where
+data GM Int v = GMI (Map.Map Int v)
+a = GMI Map.empty
+
+instance X Char where
+data GM Char v = GMI (Map.Map Char v)
+a = GMI Map.empty
+
+class Y y where
+type E e
+z :: e
+
+instance Y Int where
+type E = Int
+z = 0
